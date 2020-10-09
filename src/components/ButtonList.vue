e67a298911cc5d7f2285a41991e7b791d576d962
tgd <tgd1106@163.com>
2020-10-09 17:13:44 +0800

    feat: init

---
+++ src/components/ButtonList.vue
@@ -0,0 +1,12 @@
+<script>
+import { Button } from 'element-ui'
+export default {
+  name: 'ButtonList',
+  components: {
+    Button
+  },
+  render () {
+    return <Button>111111</Button>
+  }
+}
+</script>
